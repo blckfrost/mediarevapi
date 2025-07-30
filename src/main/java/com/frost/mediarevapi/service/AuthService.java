b27e996d328ab10cbd7162999befff3308dd4eb4
blckfrost <beataanima88@gmail.com>
2025-07-30 13:31:46 +0000

    jwt

---
+++ src/main/java/com/frost/mediarevapi/service/AuthService.java
@@ -0,0 +1,4 @@
+package com.frost.mediarevapi.service;
+
+public class AuthService {
+}
--- src/main/java/com/frost/mediarevapi/service/JWTService.java
+++ src/main/java/com/frost/mediarevapi/service/JWTService.java
@@ -1,8 +1,35 @@
 package com.frost.mediarevapi.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Date;
 
 @Service
 public class JWTService {
+
+    @Value("${jwt.secret:defaultSecret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration:86400}")
+    private int jwtExpiration;
+
+    public String generateToken(String username, String email, Integer role, String id,
+                                String pictureUrl) {
+        Date expiryDate  = new Date(System.currentTimeMillis() + jwtExpiration * 1000);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("email", email)
+                .claim("Role", role)
+                .claim("id", id)
+                .claim("pictureUrl", pictureUrl)
+                .setIssuedAt(new Date())
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
 
 }
